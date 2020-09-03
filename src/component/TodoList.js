f0b885f4f211992bffe7666d41f2e71c1cc2f6d2
Rashmi Gupta <rashmi224141@gmail.com>
2020-09-04 00:59:15 +0530

    modified todoList component

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -5,43 +5,44 @@ import Title from './Title';
 import WithDelete from './WithDelete';
 import {getNextState, getDefaultState} from '../TodoStates';
 
+const DEFAULT_TITLE = 'Todo';
 const TitleWithDelete = WithDelete(Title);
 
-const TodoList = function (props) {
-  const defaultState = {todoList: [], title: 'Todo', lastId: 0};
-  let [{todoList, title, lastId}, updateTodoInfo] = useState(defaultState);
+const TodoList = function () {
+  const [todoList, updateTodoList] = useState([]);
+  const [title, updateTitle] = useState(DEFAULT_TITLE);
+  const [id, updateId] = useState(0);
 
   const addTask = function (task) {
-    updateTodoInfo({
-      todoList: [...todoList, {task, id: lastId++, status: getDefaultState()}],
-      lastId,
-      title,
-    });
+    updateTodoList([...todoList, {task, id, status: getDefaultState()}]);
+    updateId(id + 1);
   };
 
   const removeTask = function (taskId) {
-    updateTodoInfo({
-      todoList: todoList.filter((task) => task.id !== taskId),
-      lastId,
-      title,
-    });
+    updateTodoList(todoList.filter((task) => task.id !== taskId));
   };
 
   const updateStatus = function (taskId) {
-    updateTodoInfo(() => {
+    updateTodoList(() => {
       const newTodoList = todoList.map((task) => ({...task}));
       const index = newTodoList.findIndex((task) => task.id === taskId);
       newTodoList[index].status = getNextState(newTodoList[index].status);
-      return {todoList: newTodoList, title, lastId};
+      return newTodoList;
     });
+  };
+
+  const resetTodoInfo = function (taskId) {
+    updateTodoList([]);
+    updateTitle(DEFAULT_TITLE);
+    updateId(0);
   };
 
   return (
     <div className="todoList">
       <TitleWithDelete
-        updateTitle={(title) => updateTodoInfo({todoList, title, lastId})}
+        updateTitle={(title) => updateTitle(title)}
         value={title}
-        handleDelete={() => updateTodoInfo(defaultState)}
+        handleDelete={resetTodoInfo}
       />
       <Tasks
         todoList={todoList}
