dac9dbe9ac00cc0201e516e0ed720dd66baa413d
Rashmi Gupta <rashmi224141@gmail.com>
2020-09-03 22:32:20 +0530

    changed input to the functional componenet

--- src/component/TextInput.js
+++ src/component/TextInput.js
@@ -1,34 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-class TextInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: this.props.value};
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const TextInput = function (props) {
+  const [value, setValue] = useState('');
 
-  handleChange({target}) {
-    this.setState({value: target.value});
-  }
+  const handleChange = (e) => setValue(e.target.value);
 
-  handleSubmit(event) {
-    event.preventDefault();
-    if (this.state.value) {
-      this.props.onSubmit(this.state.value);
-      this.setState({value: ''});
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    if (value) {
+      props.onSubmit(value);
+      setValue('');
     }
-  }
+  };
 
-  render() {
-    const value = this.state.value;
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" value={value} onChange={this.handleChange} />
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={value} onChange={handleChange} />
+    </form>
+  );
+};
 
 TextInput.defaultProps = {value: ''};
 
